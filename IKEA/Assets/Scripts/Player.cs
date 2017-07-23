b01112dc003c9cffc6b00ce90e8d693db719dcb8
WIN\AIT-UNF-GDC <niklas@haim.dk>
2017-07-23 18:01:06 +0200

    Frem og tilbage

--- IKEA/Assets/Scripts/Player.cs
+++ IKEA/Assets/Scripts/Player.cs
@@ -1,15 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+
+    public float turnSpeed = 30;
+    public float moveSpeed = 10;
+    public Rigidbody myRigid;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        Rigidbody myRigid = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        Move();
 	}
+
+    public void Move ()
+    {
+        transform.Translate(0, 0, moveSpeed * Time.deltaTime * Input.GetAxis("Vertical"));
+    }
+
+    public void Turn ()
+    {
+        transform.rotation(0, 0, 0);
+    }
 }
