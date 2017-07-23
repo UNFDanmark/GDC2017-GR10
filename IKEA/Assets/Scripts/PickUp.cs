b48c9ccd445d3ca3d8db0e44908910a54ff96c4d
WIN\AIT-UNF-GDC <niklas@haim.dk>
2017-07-23 20:32:36 +0200

    Arbejder på pickup

---
+++ IKEA/Assets/Scripts/PickUp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour{
+
+
+
+    void Start () {
+	}
+
+	void Update () {
+	}
+
+    void OnCollisionEnter(Collision collision){
+    
+    }
+
+
+
+
+}
--- IKEA/Assets/Scripts/Player.cs
+++ IKEA/Assets/Scripts/Player.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour{
 
+    public GameObject PlayerParent;
+    public GameObject KnottenBoksPrefab;
+    public GameObject Knotten;
     public float turnSpeed = 90;
     public float moveSpeed = 10;
     public Rigidbody myRigid;
+    private bool MyParent = false;
 
 	// Use this for initialization
 	void Start ()
@@ -28,6 +31,29 @@ public class Player : MonoBehaviour
     public void Move(float speed)
     {
         myRigid.velocity = transform.forward * speed + Vector3.up * myRigid.velocity.y;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("Knotten"))
+        {
+            Destroy(Knotten);
+            GameObject KnottenBoks = Instantiate(KnottenBoksPrefab);
+            MyParent = true;
+        }
+       
+    }
+    public void SetParent(GameObject PlayerParent)
+    {
+        if(MyParent = true)
+        {
+            //Makes the GameObject "newParent" the parent of the GameObject "player".
+            KnottenBoksPrefab.transform.parent = PlayerParent.transform;
+
+
+        }
+        
     }
 
 
